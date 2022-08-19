9cfb9a3f6e038078bef1f5dbd25ae9ac79c3e4dd
Tim Jäger <mail@timjaeger.de>
2022-08-19 23:00:42 +0200

    Add argument for headless client

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from lecture import Lecture
 WINDOW_SIZE = '1920,1080'
 
 chrome_options = Options()
-#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--window-size=%s' % WINDOW_SIZE)
 
 driver = webdriver.Chrome(options=chrome_options)
